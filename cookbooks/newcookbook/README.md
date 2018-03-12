9f6d25c52ba31733ab9e2a8b9dd1966b109587cc
venkayya <venkayyapuppala@gmail.com>
2018-03-12 05:32:22 +0000

    Adding new Cookbok to git repository

---
+++ cookbooks/newcookbook/README.md
@@ -0,0 +1,4 @@
+# newcookbook
+
+TODO: Enter the cookbook description here.
+
---
+++ cookbooks/newcookbook/recipes/default.rb
@@ -0,0 +1,5 @@
+#
+# Cookbook:: newcookbook
+# Recipe:: default
+#
+# Copyright:: 2018, The Authors, All Rights Reserved.
